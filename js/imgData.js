dd17583279fce78bc74da43235bd04976301cc40
Jonathan Craven <jcraven42@gmail.com>
2017-01-29 19:01:43 -0500

    renamed tiles

--- js/imgData.js
+++ js/imgData.js
@@ -4,17 +4,17 @@ function imgData() { };
 
 imgData.prototype = {};
 
-imgData.coll = [{ x: 0, y: 0, width: 100, height: 100, layer: 0, name: 'brick', src: 'main' },
-{ x: 100, y: 0, width: 100, height: 100, layer: 0, name: 'tiles', src: 'main' },
-{ x: 200, y: 0, width: 100, height: 100, layer: 0, name: 'flooded', src: 'main' },
-{ x: 300, y: 0, width: 100, height: 100, layer: 0, name: 'arid', src: 'main' },
-{ x: 400, y: 0, width: 100, height: 100, layer: 0, name: 'cobble', src: 'main' },
-{ x: 0, y: 100, width: 100, height:100, layer: 1, name: 'wall1Side', src: 'main' },
-{ x: 100, y: 100, width: 100, height: 100, layer: 1, name: 'wallLSide', src: 'main' },
-{ x: 200, y: 100, width: 100, height:100, layer: 1, name: 'wallUSide', src: 'main' },
-{ x: 300, y: 100, width: 100, height: 100, layer: 1, name: 'wallParSide', src: 'main' },
-{ x: 400, y: 100, width: 100, height: 100, layer: 1, name: 'wallBlock', src: 'main' },
-{ x: 0, y: 200, width: 100, height: 100, layer: 2, name: 'table', src: 'main' },
-{ x: 100, y: 200, width: 100, height:100, layer: 2, name: 'bed', src: 'main' },
-{ x: 200, y: 200, width: 100, height: 100, layer: 2, name: 'chest', src: 'main' },
-{ x: 300, y: 200, width: 100, height: 100, layer: 2, name: 'door', src: 'main' }];
+imgData.coll = [{ x: 0, y: 0, width: 100, height: 100, layer: 0, name: 'Brick', src: 'main' },
+{ x: 100, y: 0, width: 100, height: 100, layer: 0, name: 'Tiles', src: 'main' },
+{ x: 200, y: 0, width: 100, height: 100, layer: 0, name: 'Flooded', src: 'main' },
+{ x: 300, y: 0, width: 100, height: 100, layer: 0, name: 'Arid', src: 'main' },
+{ x: 400, y: 0, width: 100, height: 100, layer: 0, name: 'Cobble', src: 'main' },
+{ x: 0, y: 100, width: 100, height:100, layer: 1, name: 'Wall-1-Side', src: 'main' },
+{ x: 100, y: 100, width: 100, height: 100, layer: 1, name: 'Wall-L-Side', src: 'main' },
+{ x: 200, y: 100, width: 100, height:100, layer: 1, name: 'Wall-U-Side', src: 'main' },
+{ x: 300, y: 100, width: 100, height: 100, layer: 1, name: 'Wall-Parallel-Side', src: 'main' },
+{ x: 400, y: 100, width: 100, height: 100, layer: 1, name: 'Roof', src: 'main' },
+{ x: 0, y: 200, width: 100, height: 100, layer: 2, name: 'Table', src: 'main' },
+{ x: 100, y: 200, width: 100, height:100, layer: 2, name: 'Bed', src: 'main' },
+{ x: 200, y: 200, width: 100, height: 100, layer: 2, name: 'Chest', src: 'main' },
+{ x: 300, y: 200, width: 100, height: 100, layer: 2, name: 'Door', src: 'main' }];
